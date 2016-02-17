aa202e67696607b0071bef8f7476dd0508d71213
AleLinux <alexandrels800@gmail.com>
2016-02-16 23:16:51 -0200

    Criado Status Evento e adicionado arquivo do deltaspike para funcionamento do JTA

---
+++ src/main/java/br/com/easygame/enuns/StatusEvento.java
@@ -0,0 +1,18 @@
+package br.com.easygame.enuns;
+
+public enum StatusEvento {
+
+	ATIVO("Ativo"), 
+	CANELADO("Cancelado ");
+
+	private StatusEvento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	private final String descricao;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
